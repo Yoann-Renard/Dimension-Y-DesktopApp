f374eb86c6e3a96e757cf14bad872cc344c275e7
Yoann Renard <yoann.rnrd@gmail.com>
2022-03-15 17:24:53 +0100

    init modules & display

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,10 +1,14 @@
-import imghdr
-from pygame import image
-import os
+from ntpath import join
+from pygame import image, display
+from os.path import join
 #### IMAGES ####
-WIN_ICON = image.load(os.path.join("res", "images", "icons", "app_icon.png"))
-
+WIN_ICON = image.load(join("assets", "res", "images", "icons", "app-icon.png"))
 
 #### SOUND EFFECTS ####
 
 #### MUSICS ####
+
+#### SETUP ####
+display.set_icon(WIN_ICON)
+
+print("Assets init done.")
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import pygame
 from settings import *
+from assets import *
+
+class GameState:
+    run = True
+    clock = pygame.time.Clock()
+    active_window = "main"
 
 
+gameState = GameState()
 if "__main__" == __name__:
-    pass
+    
+    while gameState.run:
+        gameState.clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameState.run = False
+                pygame.quit()
+        
+        if gameState.active_window == "main":
+            pass
+        
+        pygame.display.update()
