ff81ab1eda81beb68e0eca2243fbb6b467a38f4d
Yoann Renard <yoann.rnrd@gmail.com>
2022-03-17 00:18:19 +0100

    add main && login draw

--- src/classes/drawer.py
+++ src/classes/drawer.py
@@ -3,7 +3,7 @@ from assets import *
 from settings import WIN_WIDTH, WIN_HEIGHT
 class Drawer:
     press_any_key_fade = 0
-    def draw_welcome(self, window, current_ticks):
+    def draw_welcome(self, window, current_ticks) -> None:
         if current_ticks % 1600 < 800:
             self.press_any_key_fade = 255
         else:
@@ -12,3 +12,9 @@ class Drawer:
         window.blit(INTRO_BG, (0,0))
         window.blit(INTRO_TEXT, ( WIN_WIDTH//2 - INTRO_TEXT.get_width()//2, WIN_HEIGHT//2 - INTRO_TEXT.get_height()))
         window.blit(PRESS_ANY_KEY, ( WIN_WIDTH//2 - PRESS_ANY_KEY.get_width()//2, WIN_HEIGHT//2 + 50))
+    
+    def draw_login(self) -> None:
+        pass
+    
+    def draw_main(self) -> None:
+        pass
